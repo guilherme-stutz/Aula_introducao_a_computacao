fa3020733ab1113bad00ec00385b385fd572a3f2
Guilherme Stutz <guilherme.stutz9@gmail.com>
2026-04-02 21:36:47 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	int x = 10, y = 5, z = 7, k = 7;
+	int idade;
+	float altura;
+	char inicial;
+	char resposta;
 	
-	printf("Os operadores relacionais sao usados para fazer comparacoes\n");
-	printf("Quando o resultado da comparacao eh FALSO retornam 0 (zero)\n");
-	printf("Quando o resultado da comparacao eh VERDADEIRO retornam 1 \n");
+	puts("=== Cadastro simples em C ===");
 	
-	printf("\nValor de x = %d   ", x);
-	printf("Valor de y = %d \n  ", y);
-	printf("Valor de z = %d   ", z);
-	printf("Valor de k = %d \n  ", k);
+	printf("Digite sua idade: ");
+	scanf("%d", &idade);
 	
-	printf("\nExemplo 1: x > y : %d", x > y);
-	printf("\nExemplo 2: x < y : %d \n", x < y);
+	printf("Digite sua altura: ");
+	scanf("%f", &altura);
 	
-	printf("\nExemplo 3: z == k : %d", z == k);
-	printf("\nExemplo 4: z != k : %d", z != k);
+	printf("Digite a inicial do seu nome: ");
+	scanf(" %c", &inicial);
 	
-	printf("\nExemplo 5: z >= x : %d", z >= x);
-	printf("\nExemplo 6: z <= x : %d", z <= x);
+	getc(stdin);
 	
-	printf("\nExemplo 7: x * z : %d", x * z);
-	printf("\nExemplo 8: x / z : %d", x / z);
+	printf("Voce gosta de programacao?(s/n) ");
+	resposta = getc(stdin);
 	
-	return (0);
+	idade = idade + 1;
+	
+	printf("\n=== Resultados ===\n");
+	printf("No proximo ano, voce tera %d anos.\n", idade);
+	printf("Sua altura informada foi %.2f m.\n", altura);
+	printf("A inicial do seu nome e %c .\n", inicial);
+	printf("Sua resposta foi %c .\n", resposta);
+	
+	puts("Programa encerrado com sucesso.");	
+	
+	return(0);
 }
